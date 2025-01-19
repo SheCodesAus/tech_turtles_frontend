e0a2fe561513d596a9864080165c2043f9af8829
AbbyKuo <abbykuo8776@gmail.com>
2025-01-19 15:32:10 +1000

    created footer component and added styles for desktop and mobile versions to styels.css

--- present_pal_frontend/src/components/Footer.jsx
+++ present_pal_frontend/src/components/Footer.jsx
@@ -1,7 +1,23 @@
-import './Footer.css';
-
 function Footer() {
-    return <div>Footer Content</div>;
+    return (
+        <footer className="footer-container">
+            <div className="footer-content">
+                <a href='#'><img src='/logos/logo-pink-bg.png' alt="PresentPal Logo"/></a>
+            </div>
+            <div className="footer-content">
+                <nav>
+                    <ul className="footer-links">
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/about">About Us</a></li>
+                        <li><a href="/contact">Contact Us</a></li>
+                    </ul>
+                </nav>
+                <p>&copy; {new Date().getFullYear()} PresentPal. All Rights Reserved.</p>
+            </div>
+
+        </footer>
+
+    );
 }
 
 export default Footer;
