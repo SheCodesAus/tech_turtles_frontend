0d582b6fe390fbd005d7eefd1b9a60ac55d5e217
haleighlv <haleighvinicombe@gmail.com>
2025-01-21 19:42:03 +1000

    header

--- present_pal_frontend/src/components/Header.jsx
+++ present_pal_frontend/src/components/Header.jsx
@@ -1,7 +1,15 @@
 import './Header.css';
 
 function Header() {
-    return <div>Header Content!</div>;
-}
+    return (
+    <div>
+        <div>
+            <img src="tech_turtles_frontend/img/logo1.png" />
+        </div>
+        <div>
+
+        </div>
+    </div>
+)}
 
 export default Header;
