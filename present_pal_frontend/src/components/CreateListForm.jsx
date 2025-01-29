d2a95f1c6ab10e7895e00e69e6093d2932b13dcb
AbbyKuo <abbykuo8776@gmail.com>
2025-01-29 19:16:42 +1000

    added home page and changed the list_name key  to name in  createlistform.

--- present_pal_frontend/src/components/CreateListForm.jsx
+++ present_pal_frontend/src/components/CreateListForm.jsx
@@ -7,7 +7,7 @@ const CreateListForm = () => {
 	const [isLoading, setIsLoading] = useState(false);
 	const [errorMessage, setErrorMessage] = useState("");
 	const [formData, setFormData] = useState({
-		list_name: "",
+		name: "",
 		description: "",
 		budget: "",
 	});
@@ -25,7 +25,7 @@ const CreateListForm = () => {
 		setIsLoading(true);
 		setErrorMessage("");
 
-		if (!formData.list_name || !formData.budget) {
+		if (!formData.name || !formData.budget) {
 			setErrorMessage("Please fill in all required fields");
 			setIsLoading(false);
 			return;
@@ -75,9 +75,9 @@ const CreateListForm = () => {
 								List Name
 							</label>
 							<input
-								id="list_name"
+								id="name"
 								type="text"
-								value={formData.list_name}
+								value={formData.name}
 								onChange={handleChange}
 								className="w-full px-4 py-2 border rounded-md"
 								style={{ ':focus': { borderColor: '#5B4B8A', outline: 'none' } }}
